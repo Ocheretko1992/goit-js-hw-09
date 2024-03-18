beac5339ee6f95ae95e0e0c9b4a3dd7c11b9b96b
Ocheretko1992 <117608037+Ocheretko1992@users.noreply.github.com>
2024-03-18 17:49:29 +0200

    to Fixed

--- src/js/2-form.js
+++ src/js/2-form.js
@@ -10,7 +10,7 @@ const changeForm = () => {
 
 const loadForm = () => {
   const sizeForm = localStorage.getItem('feedback-form-state');
-  if (saveData) {
+  if (sizeForm) {
     const { email, message } = JSON.parse(sizeForm);
     form.elements.email.value = email;
     form.elements.message.value = message;
@@ -18,25 +18,42 @@ const loadForm = () => {
 };
 
 form.addEventListener('input', changeForm);
-document.addEventListener('load', loadForm);
+window.addEventListener('load', loadForm);
 
-form.addEventListener('submit', event => {
+
+form.addEventListener('submit', validForm)
+  
+  function validForm(event){
   event.preventDefault();
   
   localStorage.removeItem('feedback-form-state');
 
   if (form.elements.email.value === '') {
-    alert('Please enter your email');
+    alert('Введіть будь ласка свою електронну адресу (email)');
+  } else if(form.elements.message.value === '') {
+    alert('Введіть будь ласка своє повідомлення (message)');
   }
 
-  if (form.elements.message.value === '') {
-    alert('Please enter your message');
-  }
-
-  console.log({
+  if (form.elements.email.value.trim() && form.elements.message.value.trim()) {
+    console.log({
     email: form.elements.email.value.trim(),
-    message: form.elements.message.value.trim(),
-  });
-
+    message: form.elements.message.value.trim()});
+  }
+  
+ if (form.elements.email.value.trim() && form.elements.message.value.trim()) {
   form.reset();
-});
+ }
+  
+};
+
+
+
+
+
+
+
+
+
+
+
+
